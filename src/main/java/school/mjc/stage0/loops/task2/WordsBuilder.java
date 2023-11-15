56c3557e2a5cf846a31ad52485cd28af6f13d65e
Tatsiana Kastrubai <tatsiana.kastrubai@gmail.com>
2023-11-15 15:59:44 +0300

    feat: complete task2 module4.2 v.4

--- src/main/java/school/mjc/stage0/loops/task2/WordsBuilder.java
+++ src/main/java/school/mjc/stage0/loops/task2/WordsBuilder.java
@@ -2,6 +2,13 @@ package school.mjc.stage0.loops.task2;
 
 public class WordsBuilder {
     public void buildPhrase(char... chars) {
+        int whileCounter = 0;
+        String phrase = "";
+        while (whileCounter < chars.length){
+            String temp = Character.toString(chars[whileCounter]);
+            System.out.print(phrase.concat(temp));
+            whileCounter++;
+        }
 
     }
 }
